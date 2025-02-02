aa72fb7b7d341ae82d5d8fcc92c6917115bb15b5
nebiyou27 <neberlio21@gmail.com>
2025-02-02 20:20:58 +0300

    Add DBT models, documentation, and tests

---
+++ my_project/target/compiled/my_project/models/raw_data_transformation.sql
@@ -0,0 +1,3 @@
+SELECT *
+FROM telegram_messages
+WHERE date IS NOT NULL
---
+++ my_project/target/run/my_project/models/raw_data_transformation.sql
@@ -0,0 +1,9 @@
+
+  create view "telegram_data"."public"."raw_data_transformation__dbt_tmp"
+    
+    
+  as (
+    SELECT *
+FROM telegram_messages
+WHERE date IS NOT NULL
+  );
